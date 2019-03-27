f28268e79cdd6cbf5448b0287805ad0597babd32
Aniket Patel <aniketupatel1@gmail.com>
2019-03-27 05:33:33 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,14 @@ require_relative '../lib/operations.rb'
 
 RSpec.configure do |config|
   # config here
-end
+  # context.example is deprecated, but RSpec.current_example is not
+    # available until RSpec 3.0.
+    fetch_current_example = RSpec.respond_to?(:current_example) ?
+      proc { RSpec.current_example } : proc { |context| context.example }
+
+    c.before(:example) do
+      example = fetch_current_example.call(self)
+
+      # ...
+    end
+  end
